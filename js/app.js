c54e13ff40bcc95b54ff53cba9b870c4fd79d5f0
Miguel Palau <mpalauzarza@gmail.com>
2016-01-13 11:44:51 -0600

    Update 2016-01-13T17:44:09.757Z

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*!
+ * Código por @mpalau
+ * Parte del Framework Manila para Don Mango / GeekyBytes
+ * Version 1.5
+ * Copyright 2015
+ *
+ * http://miguel.click/ 😜
+ */
+(function(){!function(){return $(".header__toggle").click(function(){return $(".header__toggle--burger").toggleClass("is-active"),$(".header__mobile-menu").toggleClass("is-open")})}()}).call(this),function(){console.log("La vida es más que bella, es hermosa")}.call(this);
