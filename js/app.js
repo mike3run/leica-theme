a450eb5be081e32f1081d15bf811280687e4940d
Miguel Palau <mpalauzarza@gmail.com>
2016-01-18 20:36:24 -0600

    Update 2016-01-19T02:36:19.679Z

--- js/app.js
+++ js/app.js
@@ -6,4 +6,4 @@
  *
  * http://miguel.click/ 😜
  */
-(function(){!function(){return $(".menu__toggle").click(function(){return $(".menu__toggle--burger").toggleClass("is-active"),$(".menu__mobile-menu").toggleClass("is-open")})}()}).call(this),function(){console.log("La vida es más que bella, es hermosa")}.call(this);
+(function(){!function(){return $(".menu__toggle").click(function(){return $(".menu__toggle--burger").toggleClass("is-active"),$(".menu__mobile-menu").toggleClass("is-open")})}(),function(){return $("code").each(function(e,n){return hljs.highlightBlock(n),console.log("Yeah")})}()}).call(this),function(){console.log("La vida es más que bella, es hermosa")}.call(this);
