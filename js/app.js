394de5a1674f7e8f6d230a4f278c407e2025f754
Miguel Palau <mpalauzarza@gmail.com>
2016-01-19 21:47:55 -0600

    Update 2016-01-20T03:47:50.292Z

--- js/app.js
+++ js/app.js
@@ -6,4 +6,4 @@
  *
  * http://miguel.click/ 😜
  */
-(function(){!function(){return $(".menu__toggle").click(function(){return $(".menu__toggle--burger").toggleClass("is-active"),$(".menu__mobile-menu").toggleClass("is-open")})}(),function(){return $("code").each(function(e,n){return hljs.highlightBlock(n),console.log("Yeah")})}()}).call(this),function(){console.log("La vida es más que bella, es hermosa")}.call(this);
+(function(){!function(){return $(".menu__toggle").click(function(){return $(".menu__toggle--burger").toggleClass("is-active"),$(".menu__mobile-menu").toggleClass("is-open")})}()}).call(this),function(){var e;!function(){return $("code").each(function(e,n){return hljs.highlightBlock(n)})}(),$("code").hasClass("hljs")?(e=$(".hljs").css("background"),$("pre").css("background",e)):console.log("No hay código que estilizar nenas!")}.call(this),function(){var e,n,l;null!==$(".header").attr("poster")&&(e=$(".header"),n=$(".header").attr("poster"),l="linear-gradient(rgba(141,131,79,0.50) 0%, rgba(37,27,27,0.50) 80%) , url('"+n+"') center / cover",e.css("background",l))}.call(this),function(){console.log("La vida es más que bella, es hermosa")}.call(this);
